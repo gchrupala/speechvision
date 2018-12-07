9ca0678f7f5ca918a09461f27ba9364b1259a2e4
Grzegorz Chrupała <pitekus@gmail.com>
2018-12-07 14:42:33 +0100

    forced_align

--- forced_align.py
+++ forced_align.py
@@ -22,12 +22,12 @@ def align(audiopath, transcript):
                                    conservative=False)
         return aligner.transcribe(wavfile, progress_cb=on_progress, logging=logging)
 
-def align_val(root=ROOT):
-    coco = json.load(open("/home/gchrupala/repos/reimaginet/data/synthetically-spoken-coco/dataset.json"))
+def align_val(root=ROOT, dataset_path="/home/gchrupala/repos/reimaginet/data/synthetically-spoken-coco/dataset.json"):
+    data = json.load(open(dataset_path))
     log_level = "INFO"
     logging.getLogger().setLevel(log_level)
     with open("dataset.val.fa.json", 'w') as fa:
-        for image in coco['images']:
+        for image in data['images']:
             if image['split'] == 'val':
                 for sent in image['sentences']:
                     audiopath = "{}/{}.mp3".format(root, sent['sentid'])
@@ -38,12 +38,12 @@ def align_val(root=ROOT):
                     fa.write(json.dumps(result))
                     fa.write("\n")
 
-def index(t):
-    """Return index into the recurrent state of COCO model given timestep
+def index(t, stride=3):
+    """Return index into the recurrent state of speech model given timestep
     `t`.
 
     """
-    return (t//10+6)//3
+    return (t//10+6)//stride
 
 def phones(utt):
     """Return sequence of phoneme labels associated with start and end
@@ -80,7 +80,8 @@ def phoneme_states(alignments, reps, aggregate):
 def phoneme_train_data(alignment_path="data/coco/dataset.val.fa.json",
                   dataset_path="data/coco/dataset.json",
                   model_path="models/coco-speech.zip",
-                  max_size=5000):
+                  max_size=5000,
+                  directory="."):
     """Generate data for training a phoneme decoding model."""
     import imaginet.data_provider as dp
     import imaginet.task as task
@@ -103,13 +104,13 @@ def phoneme_train_data(alignment_path="data/coco/dataset.val.fa.json",
     
     logging.info("Extracting MFCC examples")
     data_state =  [phoneme for (utt, sent) in data_filter for phoneme in slices(utt, sent['audio']) ]
-    save_fa_data(data_state, prefix="mfcc_")
+    save_fa_data(data_state, prefix=directory + "/mfcc_")
     
     logging.info("Extracting convo states")
     states = audiovis.conv_states(model_rhn, [ sent['audio'] for utt,sent in data_filter ])
     data_state =  [phoneme for i in range(len(data_filter))
                    for phoneme in slices(data_filter[i][0], states[i], index=index) ]
-    save_fa_data(data_state, prefix="conv_")
+    save_fa_data(data_state, prefix=directory + "/conv_")
     
     logging.info("Extracting recurrent layer states")
     states = audiovis.layer_states(model_rhn, [ sent['audio'] for utt,sent in data_filter ], batch_size=32)
@@ -118,7 +119,7 @@ def phoneme_train_data(alignment_path="data/coco/dataset.val.fa.json",
             return x[:,layer,:].mean(axis=0)
         data_state =  [phoneme for i in range(len(data_filter))
                        for phoneme in slices(data_filter[i][0], states[i], index=index, aggregate=aggregate) ]
-        save_fa_data(data_state, prefix="rec{}_".format(layer))
+        save_fa_data(data_state, prefix=directory + "/rec{}_".format(layer))
         
 def save_fa_data(data_state, prefix=""):
     y, X = zip(*data_state)
@@ -127,27 +128,28 @@ def save_fa_data(data_state, prefix=""):
     numpy.save(prefix + "features.npy", X)
     numpy.save(prefix + "phonemes.npy", y)
 
-def phoneme_test_data(mp3_path="ganong", 
-                      info_path="ganong-coco/ganong.csv", 
-                      rep_path="ganong-coco", 
-                      fa_path='ganong-fa.json'):
-    """Generate data for ganong experiment"""
+def run_gentle(mp3_path):
     import pandas as pd
     logging.basicConfig(level=logging.INFO)
-    data = pd.read_csv(info_path)
-    try:
-        fa = json.load(open(fa_path))
-    except FileNotFoundError:
-        
-        fa = []    
-        for i in range(data.shape[0]):
+    data = pd.read_csv(mp3_path + "/info.csv")
+    fa_path = "{}/fa.json".format(mp3_path)
+    fa = []    
+    for i in range(data.shape[0]):
             transcript = data.iloc[i]['transcript']
-            path = data.iloc[i]['path']
+            path = mp3_path + "/" + data.iloc[i]['path']
             logging.info("Processing file {}".format(path))
             result = json.loads(align(path, transcript).to_json())
             result['path'] =  path
             fa.append(result)
-        json.dump(fa, open(fa_path, 'w'))
+    json.dump(fa, open(fa_path, 'w'))
+    
+def phoneme_test_data(mp3_path="ganong", rep_path="ganong-coco"): 
+    """Generate data for ganong experiment"""
+    try:
+        fa = json.load(open(fa_path))
+    except:
+        run_gentle(mp3_path)
+        fa = json.load(open(fa_path))
     FA = dict((utt['path'], utt) for utt in fa)
     alignment = [ FA[data.iloc[i]['path']] for i in range(data.shape[0]) ]
     logging.info("Extracting MFCC examples")
@@ -193,4 +195,28 @@ def decoding(rep, directory="."):
     z = np.load(directory  + "/" + "test_" + rep + "_phonemes.npy")
     test_score = 1-model.score(W, z)
     return (val_score, test_score)
+
+
+def run_decoding(paths, mp3path, rep="mfcc", model_type='coco'):
+    import activations as A
+    audio_paths = [ line.strip() for line in open(paths)]
+    model = "models/{}-speech.zip".format(model_type)
+    directory = "{}-{}".format(mp3path, model_type)
+    A.save_activations(audio_paths, model, 
+                       directory + "/mfcc.npy", 
+                       directory + "/conv_states.npy", 
+                       directory + "/states.npy",
+                       directory + "/embeddings.npy", 
+                       accel=model_type == 'flickr8k', 
+                       nfft=1024)    
+    if model_type == 'coco':
+        phoneme_train_data(alignment_path="data/coco/dataset.val.fa.json",
+                      dataset_path="data/coco/dataset.json",
+                      model_path=model,
+                      max_size=5000,
+                      directory=directory)
+    else:
+        raise NotImplementedError
+    phoneme_test_data(mp3_path=mp3path, rep_path=directory)
+    print(decoding(rep, directory=directory))
 
